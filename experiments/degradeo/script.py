2f297f7c2e1156bdec3c5aea5c9b095433bd1594
Ariane Mora <ariane.n.mora@gmail.com>
2025-04-11 19:33:46 -0700

    Development to enable v high throughput

--- experiments/degradeo/script.py
+++ experiments/degradeo/script.py
@@ -10,10 +10,12 @@ files = os.listdir('data/')
 for f in files:
     if f != 'train-00001-of-00032':
         cmd = f'annotatee fasta /disk1/ariane/vscode/annotate-e/experiments/degradeo/data/{f}/input_df.csv /disk1/ariane/vscode/annotate-e/experiments/degradeo/Uniprot_reviewed_catalytic_activity_06032025.fasta --methods blast,foldseek  --foldseek-db /disk1/share/software/foldseek/structures/pdb/pdb --output-folder /disk1/ariane/vscode/annotate-e/experiments/degradeo/output/ --run-name omgprot50_{f}'
+        os.system(cmd)
+        break
         runs.append(cmd)
         count += 1
         if count == 2:
             break
-num_threads = 2
-pool = ThreadPool(num_threads)
-output_filenames = pool.map(run, runs)
+# num_threads = 2
+# pool = ThreadPool(num_threads)
+# output_filenames = pool.map(run, runs)
